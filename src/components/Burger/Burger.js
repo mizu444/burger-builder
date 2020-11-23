3f29474333bef63e85b7bb96aea8c21afde2a97d
Mizu Cuchty <m.cuchty@gmail.com>
2020-11-23 18:18:04 +0100

    add console log

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,15 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+  // console.log("Props Ingredients: ", props.ingredients);
+  // console.log(Object.keys(props.ingredients));
   let transformedIngredients = Object.keys(props.ingredients)
     .map((igKey) => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />;
       });
     })
+
     .reduce((arr, el) => {
       return arr.concat(el);
     }, []);
